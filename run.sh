7c549e39d6220fcd6bc6a0a140d1f5ca716ce907
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-06-26 22:33:50 +0200

    Avoid the same PKB file to be used by more than one instance of Opencal at the same time.

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source activate opencal-dev
-
-
-# TODO: WORKAROUND https://forum.qt.io/topic/54802/linux-application-does-not-accept-keyboard-input/4
-QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb ./opcgui/qt/main.py
